09e19200a165c64ec414be5ce76b38379b5cc95c
Damian Skowroński <skowronskid@icloud.com>
2023-07-20 18:08:54 +0200

    finished working todo calendar

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 //calendar reference
 const calendar = document.getElementById('calendar');
+// new event div
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
 
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
 //weekdays
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
@@ -53,9 +58,20 @@ function load() {
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
+        const dayString = `${month+1}/${i - paddingDays}/${year}`;
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays;
-            daySquare.addEventListener('click', () => console.log('click'));
+
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if (eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
         } else {
             daySquare.classList.add('padding');
         }
@@ -64,8 +80,49 @@ function load() {
 }
 
 function openModal(date) {
-    
+    clicked = date;
+
+    const eventForDay = events.find(e => e.date === clicked);
+
+    if (eventForDay) {
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
+    } else {
+        newEventModal.style.display = 'block';
+    }
+    backDrop.style.display = 'block';
 }
+function closeModal() {
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    } else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+}
+
 
 function initButtons() {
     document.getElementById('backButton').addEventListener('click', () => {
@@ -73,9 +130,16 @@ function initButtons() {
         load();
     });
     document.getElementById('nextButton').addEventListener('click', () => {
-        nav--;
+        nav++;
         load();
     })
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+    
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
+    
+
 }
 
 initButtons();
